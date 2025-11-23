039d40885a6ab3790f7e20fe8c3646381facfefe
pustotnik-ra <rusvosem@gmail.com>
2025-11-23 22:23:08 +0500

    Fix syntax error in quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -20,7 +20,7 @@ const config: QuartzConfig = {
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
-      fontOrigin: "googleFonts",",
+      fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
         header: "Schibsted Grotesk",
